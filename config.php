6c3ba554ff368f4438ac96fc6bed6d79c4889699
“CDylan-source” <chapuis.dylan18@gmail.com>
2021-06-10 10:05:45 +0200

    suppr d'un élément indésirable

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=clementp_quizzoot;charset=utf8', 'clementp', '6HnmyOxlqQj+mw=='); 
+	$bdd = new PDO('mysql:host=localhost;dbname=clementp_quizzoot;charset=utf8', 'clementp', ''); 
         echo 'banane';
 }
 catch (Exception $e)
